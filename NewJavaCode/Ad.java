b641490ff0f0dac7c9550e4dfec5dddcdb741097
skeeter144 <alexander.n.finney@gmail.com>
2015-04-01 21:35:52 -0500

    Create Ad.java

---
+++ NewJavaCode/Ad.java
@@ -0,0 +1,27 @@
+import java.io.Serializable;
+
+
+public class Ad implements Serializable{
+
+	private static final long serialVersionUID = 5270221566768503474L;
+	
+	private AdCompany owner;
+	private String adName;
+	
+	public Ad(AdCompany owner, String adName){
+		this.owner = owner;
+		this.adName = adName;
+	}
+	
+	public AdCompany getOwner(){
+		return owner;
+	}
+	
+	public String getAdName(){
+		return adName;
+	}
+	
+	public void setAdName(String adName){
+		this.adName = adName;
+	}
+}
