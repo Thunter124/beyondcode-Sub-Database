6d3a4cd0794570b1cc33e780eb067ee682baa00b
skeeter144 <alexander.n.finney@gmail.com>
2015-04-01 21:36:16 -0500

    Create AdCompany.java

---
+++ NewJavaCode/AdCompany.java
@@ -0,0 +1,34 @@
+import java.io.Serializable;
+
+
+public class AdCompany implements Serializable{
+
+	private static final long serialVersionUID = -5924130455293319486L;
+	private String companyName;
+	private String address;
+	
+	public AdCompany(String companyName, String address){
+		this.companyName = companyName;
+		this.address = address;
+	}
+	
+	public void setCompanyName(String companyName){
+		this.companyName = companyName;
+	}
+	
+	public String getCompanyName(){
+		return companyName;
+	}
+	
+	public void setCompanyAddress(String address){
+		this.address = address;
+	}
+	
+	public String getCompanyAddress(){
+		return address;
+	}
+	
+	
+	
+	
+}
